af72c90eaf578c6aee717fd6c3a3eb869e5af0b9
uuip <uuip@users.noreply.github.com>
2024-04-25 17:55:38 +0800

    添加yaml作为配置文件

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 from pydantic import Field, computed_field
-from pydantic_settings import SettingsConfigDict, BaseSettings
+from pydantic_settings import SettingsConfigDict, BaseSettings, PydanticBaseSettingsSource, YamlConfigSettingsSource
 
 _env_file = Path(__file__).parent / ".env"
 
@@ -25,3 +25,20 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class YamlSettings(BaseSettings):
+    model_config = SettingsConfigDict(yaml_file=..., extra="ignore")
+
+    db: str = Field(alias="db_url")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return init_settings, env_settings, YamlConfigSettingsSource(settings_cls), file_secret_settings
