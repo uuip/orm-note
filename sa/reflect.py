6c03d5856b97eb2ed0b51276dfaa2c2ae5b8d4e6
uuip <uuip@users.noreply.github.com>
2024-08-23 16:42:13 +0800

    add other methods

--- sa/reflect.py
+++ sa/reflect.py
@@ -9,3 +9,24 @@ AutoBase = automap_base()
 
 AutoBase.prepare(autoload_with=db)
 models = AutoBase.classes
+
+# # ==================
+# metadata = MetaData()
+# metadata.reflect(bind=db)
+# model = metadata.tables["users"]
+# # ==================
+# metadata = MetaData()
+# original = Table(from_table, metadata, autoload_with=db)
+# new_table = Table(to_table, metadata)
+#
+# for col in original.columns:
+#     if col.name == "id":
+#         continue
+#     new_table.append_column(col.copy())
+# new_table.append_column(Column("id", BigInteger, primary_key=True))
+# metadata.drop_all(bind=db, tables=[new_table])
+# metadata.create_all(bind=db, tables=[new_table])
+#
+#
+# class NewTableModel(declarative_base(metadata=metadata)):
+#     __table__ = new_table
