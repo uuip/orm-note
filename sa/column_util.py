4de131db147b10ead45d1cb6127862578bcc662b
uuip <uuip@users.noreply.github.com>
2024-09-24 01:09:55 +0800

    Update

--- sa/column_util.py
+++ sa/column_util.py
@@ -1,5 +1,5 @@
 def model_colunm_map(obj):
-    return {k: getattr(obj, k) for k, v in obj.__mapper__.c.items()}
+    return {model_field: getattr(obj, model_field) for model_field, c in obj.__mapper__.c.items()}
 
 
 def db_column_map(obj):
